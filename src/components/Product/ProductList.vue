8ffc8621ace3fd7545dccad2cec84e7266cdfe07
Artem71 <fich71@mail.ru>
2020-11-18 22:10:36 +0300

    add vuex

--- src/components/Product/ProductList.vue
+++ src/components/Product/ProductList.vue
@@ -4,7 +4,7 @@
       <v-flex xs12 sm6 offset-sm3>
         <h1 class="text--secondary mb3">My Products</h1>
         <v-card
-          v-for="(product, i) in products"
+          v-for="(product, i) in myProducts"
           :key="i"
           class="elevation-10 mb-2"
         >
@@ -39,31 +39,10 @@
 
 <script>
 export default {
-  data: () => ({
-    products: [
-      {
-        id: '1',
-        title: 'Lenovo Legoin Y520',
-        vendor: 'Lenovo',
-        color: 'black',
-        material: 'metal/plastic',
-        description: 'Intel Core i5 7300HQ 2500 MHz/15.6"/1920x1080/8Gb/1000Gb HDD/DVD нет/NVIDIA GeForce GTX 1050, 4096 МБ/Wi-Fi/Bluetooth/Win 10 Home',
-        price: 784,
-        promo: false,
-        imageSrc: 'https://image.ibb.co/fZzq1o/Lenovo_Legion_Y520.jpg'
-      },
-      {
-        id: '2',
-        title: 'Asus FX503VD',
-        vendor: 'Asus',
-        color: 'white',
-        material: 'plastic',
-        description: 'Intel Core i5 7300HQ 2500 MHz/15.6"/1920x1080/8Gb/256Gb SSD/DVD нет/NVIDIA GeForce GTX 1050/Wi-Fi/Bluetooth/Windows 10 Home',
-        price: 984,
-        promo: true,
-        imageSrc: 'https://image.ibb.co/cpScgo/ASUS_FX503_VD.jpg'
-      },
-    ]
-  })
+  computed: {
+    myProducts() {
+      return this.$store.getters.myProducts
+    }
+  }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import VueRouter from 'vue-router'
 import router from './router'
+import store from './store'
 import '@babel/polyfill'
 
 Vue.use(VueRouter)
@@ -12,5 +13,6 @@ Vue.config.productionTip = false
 new Vue({
   vuetify,
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
